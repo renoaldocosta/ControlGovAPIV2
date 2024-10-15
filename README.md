2793263173bd3b3edf8c185160a56f2bc504f14b
renoaldo <renoaldo@hotmail.com>
2024-10-15 20:23:01 -0300

    first commit

--- README.md
+++ README.md
@@ -26,3 +26,4 @@ uvicorn app:app --reload
 Now you can load http://localhost:8000/docs in your browser ... but there won't be much to see until you've inserted some data.
 
 If you have any questions or suggestions, check out the [MongoDB Community Forums](https://developer.mongodb.com/community/forums/)!
+# ControlGovAPIV2
--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = FastAPI(
     summary="A sample application showing how to use FastAPI to add a ReST API to a MongoDB collection.",
 )
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
-db = client.college
+db = client.get_database("college")
 student_collection = db.get_collection("students")
 
 # Represents an ObjectId field in the database.
